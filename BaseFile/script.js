042cbacf6b51dc2a37d737e62050762b0396f0fa
Mehmet Akif Yegin <94207328+makifyegin@users.noreply.github.com>
2023-07-30 22:39:44 +0000

    create a first sceneraio

---
+++ BaseFile/script.js
@@ -0,0 +1,38 @@
+
+
+
+//  Scene
+const scene =  new THREE.Scene();
+
+// Red cube
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({color: 'red'})
+
+const mesh = new THREE.Mesh(geometry, material)
+
+scene.add(mesh)
+
+
+
+//Sizes
+
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+
+const camera = new THREE.PerstectiveCamera(75, sizes.width/sizes.height);
+scene.add(camera);
+
+
+// Renderer
+const canvas = document.querySelector('.webgl')
+console.log(canvas)
+const renderer = new THREE.WebGLRenderer({
+    canvas
+})
+
+renderer.setSize(sizes.width, sizes.height)
