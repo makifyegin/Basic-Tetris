5ab1ffab513f055206b8b0b4ab509cec7d264976
Mehmet Akif Yegin <94207328+makifyegin@users.noreply.github.com>
2023-07-30 23:06:26 +0000

    created first scenario in three.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const sizes = {
 
 // Camera
 
-const camera = new THREE.Perspective(75, sizes.width/sizes.height);
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height);
 
 camera.position.z = 3
 scene.add(camera);
